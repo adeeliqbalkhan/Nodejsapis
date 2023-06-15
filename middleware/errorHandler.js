8f938a5760d2099de631505e10fcc9705b2d5db2
adeeliqbalkhan <128506298+adeeliqbalkhan@users.noreply.github.com>
2023-06-15 03:40:11 -0700

    stack trace, html.index, extra packages removed

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,19 +4,19 @@ const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode ? err.statusCode : 500;
     switch (statusCode) {
         case constants.VALIDATION_ERROR:
-            res.json({ title: "VALIDATION_ERROR", message: err.message, stackTrace: err.stack })
+            res.json({ title: "VALIDATION_ERROR", message: err.message })
             break;
         case constants.NOT_FOUND:
-            res.json({ title: "NOT_FOUND", message: err.message, stackTrace: err.stack })
+            res.json({ title: "NOT_FOUND", message: err.message })
             break;
         case constants.UNAUTHORIZED:
-            res.json({ title: "UNAUTHORIZED", message: err.message, stackTrace: err.stack })
+            res.json({ title: "UNAUTHORIZED", message: err.message })
             break;
         case constants.FORBIDDEN:
-            res.json({ title: "FORBIDDEN", message: err.message, stackTrace: err.stack })
+            res.json({ title: "FORBIDDEN", message: err.message })
             break;
         case constants.SERVER_ERROR:
-            res.json({ title: "SERVER_ERROR", message: err.message, stackTrace: err.stack })
+            res.json({ title: "SERVER_ERROR", message: err.message })
             break;
         default:
             console.log("No Error, All good!")
