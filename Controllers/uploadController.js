2fbb502bc500a88b5409bbc854005c079b288da8
adeeliqbalkhan <128506298+adeeliqbalkhan@users.noreply.github.com>
2023-06-21 05:00:12 -0700

    new s3

--- Controllers/uploadController.js
+++ Controllers/uploadController.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const multer = require('multer');
-const AWS = require('aws-sdk');
+const { S3Client } = require("@aws-sdk/client-s3")
 
-// Configure AWS SDK
-AWS.config.update({
-    accessKeyId: 'YOUR_ACCESS_KEY_ID',
-    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-    region: 'YOUR_S3_REGION'
+//instance of S3Client
+const s3Client = new S3Client({
+    region: 'YOUR_S3_REGION',
+    credentials: {
+        accessKeyId: 'YOUR_ACCESS_KEY_ID',
+        secretAccessKey: 'YOUR_SECRET_ACCESS_KEY'
+    }
 });
 
-const s3 = new AWS.S3();
+const s3 = new S3Client({});
+
 
 function uploadToS3(file) {
     const params = {
@@ -19,28 +22,22 @@ function uploadToS3(file) {
     };
 
     return new Promise((resolve, reject) => {
-        s3.upload(params, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
+        s3Client.send(new PutObjectCommand(params))
+            .then(data => resolve(data))
+            .catch(err => reject(err));
     });
 }
 
 // Use the Multer middleware for file upload
+
 const uploadImage = (req, res) => {
-    if (err) {
-        return res.status(500).json({ message: "Error Uploading file!" });
-    }
     try {
         const uploadedFile = uploadToS3(req.file);
         res.status(200).send("File uploaded successfully");
-
     } catch (error) {
         res.status(500).send("Error uploading file to S3");
     }
 };
 
 module.exports = { uploadImage }
+//
